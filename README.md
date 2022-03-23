e4b386c4c7bca87bd9bca393ccbe603f5c9fef3c
dymafr <erwan1309@hotmail.com>
2022-03-23 10:24:54 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# vitejs-vite-u9xev4
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/vitejs-vite-u9xev4)
---
+++ src/features/admin/components/ProductForm.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="card">
+    <h1>Formulaire</h1>
+  </div>
+</template>
+
+<style scoped lang="scss">
+.card {
+  width: 100%;
+  max-width: 500px;
+}
+</style>
